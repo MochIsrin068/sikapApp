6df4391e994a0158b4ea7092564909adc0a66d42
IyanSR <ikhyan7@gmail.com>
2019-11-11 12:22:53 +0700

    Add Splasy Screen

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/iyansr/DevTools/flutter"
-export "FLUTTER_APPLICATION_PATH=/media/bd381ac5-aeee-49a1-bda4-0f2c4ad0bd9e/Flutter/sikapApp"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\Feelance\Flutter\sikapApp"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/iyansr/DevTools/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sikap/utils/dashboard_provider%20copy.dart';
@@ -7,20 +10,55 @@ import 'package:sikap/utils/keberangkatan_provider.dart';
 import 'package:sikap/utils/kedatangan_provider.dart';
 import 'package:sikap/views/Home.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
+// void main() => runApp(SplahScreen());
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
         title: 'SIKAP - Sistem Informasi Aktivitas Kapal Perikanan',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           fontFamily: 'Poppins',
         ),
-        home: MultiProvider(
+  home: SplahScreen(),
+  
+));
+
+class SplahScreen extends StatefulWidget {
+  @override
+  _SplahScreenState createState() => _SplahScreenState();
+}
+
+class _SplahScreenState extends State<SplahScreen> {
+  startTime() async {
+    var _duration = Duration(seconds: 3);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage(){
+    Navigator.of(context).pushReplacement(CupertinoPageRoute(builder: (context) => MyApp()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('images/splash.jpg'),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
           providers: [
             ChangeNotifierProvider<KapalProvider>(
                 builder: (_) => KapalProvider()),
@@ -34,6 +72,6 @@ class MyApp extends StatelessWidget {
                 builder: (_) => IkanProvider()),
           ],
           child: Home(),
-        ));
+        );
   }
 }
